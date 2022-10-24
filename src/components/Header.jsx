eed3ed076da02c7937d92ef13ea85621ad03465b
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-10-24 09:05:48 -0300

    add classes e import style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,37 +2,46 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import md5 from 'crypto-js/md5';
+import '../styles/header.scss';
+import { GiRoundStar } from 'react-icons/gi';
+import { BsCheckLg } from 'react-icons/bs';
 
 class Header extends React.Component {
   render() {
     const { name, gravatarEmail, score, assertions } = this.props;
 
     return (
-      <div>
-        <img
-          src={ `https://www.gravatar.com/avatar/${md5(gravatarEmail).toString()}` }
-          alt="avatar"
-          data-testid="header-profile-picture"
-        />
-        <p
-          name="name"
-          value="name"
-          data-testid="header-player-name"
-        >
-          {name}
-        </p>
-        <p
-          name="currency"
-          value="score"
-          data-testid="header-score"
-        >
-          { score }
-        </p>
-        <p>
-          Assertions:
-          {' '}
-          {assertions}
-        </p>
+      <div className="header">
+        <div>
+          <img
+            src={ `https://www.gravatar.com/avatar/${md5(gravatarEmail).toString()}` }
+            alt="avatar"
+            data-testid="header-profile-picture"
+          />
+          <p
+            name="name"
+            value="name"
+            data-testid="header-player-name"
+          >
+            {name}
+          </p>
+        </div>
+        <div className="score">
+          <GiRoundStar />
+          <p
+            name="currency"
+            value="score"
+            data-testid="header-score"
+          >
+            { `Score: ${score}` }
+          </p>
+        </div>
+        <div className="assertions">
+          <BsCheckLg />
+          <p>
+            { `Assertions: ${assertions}` }
+          </p>
+        </div>
       </div>
     );
   }
