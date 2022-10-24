291aa26b4d9d22f89cd4905588e68b5fde6772e8
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-10-24 09:08:58 -0300

    add setting action

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,7 @@
 export const USER_LOGIN = 'USER_LOGIN';
 export const USER_TOKEN = 'USER_TOKEN';
 export const USER_SCORE = 'USER_SCORE';
+export const SETTING = 'SETTING';
 
 export const userLogin = (state) => ({
   type: USER_LOGIN,
@@ -9,5 +10,10 @@ export const userLogin = (state) => ({
 
 export const addScore = (state) => ({
   type: USER_SCORE,
+  payload: state,
+});
+
+export const setting = (state) => ({
+  type: SETTING,
   payload: state,
 });
